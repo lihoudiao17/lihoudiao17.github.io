8d2c0e9f306bad128b406a96dd109780e95f0a02
qilv-lattice <mymdphdcandidate2@gmail.com>
2026-01-05 17:55:22 -0800

    feat: 移动优先响应式重构 - 添加 base.css 和 desktop.css，优化三端显示

--- script.js
+++ script.js
@@ -118,9 +118,9 @@ function toggleMode() {
 
     // 修改按钮文字
     if (card.classList.contains('horizontal-mode')) {
-        btn.innerText = "竖排模式"; // 当前是横排，提示用户点击变竖排
+        btn.innerHTML = "竖排<br>模式"; // 当前是横排，提示用户点击变竖排
     } else {
-        btn.innerText = "横排模式"; // 当前是竖排，提示用户点击变横排
+        btn.innerHTML = "横排<br>模式"; // 当前是竖排，提示用户点击变横排
     }
 }
 
